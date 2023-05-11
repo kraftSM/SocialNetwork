b3e8e4392905e5854332bd6ecb4158243f2e65eb
sergey <KraftSM@yandex.ru>
2023-05-11 14:02:10 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.Sqlite;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DAL.Repositories
+{
+    public class BaseRepository
+    {
+        protected T QueryFirstOrDefault<T>(string sql, object parameters = null)
+        {
+            using (var connection = CreateConnection())
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<T>(sql, parameters);
+            }
+        }
+
+        protected List<T> Query<T>(string sql, object parameters = null)
+        {
+            using (var connection = CreateConnection())
+            {
+                connection.Open();
+                return connection.Query<T>(sql, parameters).ToList();
+            }
+        }
+
+        protected int Execute(string sql, object parameters = null)
+        {
+            using (var connection = CreateConnection())
+            {
+                connection.Open();
+                return connection.Execute(sql, parameters);
+            }
+        }
+
+        private IDbConnection CreateConnection()
+        {
+            return new SqliteConnection("Data Source = DAL/DB/social_network.db; Version = 3");
+        }
+    }
+}
